34b43fd46f49610b1194860421447193a44269fa
Jan Forst <ensonador@gmail.com>
2017-07-20 11:03:59 +0200

    Adding Configuration service

---
+++ src/services/configuration.ts
@@ -0,0 +1,8 @@
+export default class Configuration {
+  url: string,
+  allowUrlEdit: boolean = true,
+  allowSwithTheme: boolean = true,
+  defaultDarkTheme: boolean = false,
+  allowEditProxy: boolean = true,
+  defaultTitle: string = 'API'
+}
