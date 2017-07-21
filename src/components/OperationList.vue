0e57b7a21b3ad94634e773e637d39e3f5e53bd37
Jan Forst <ensonador@gmail.com>
2017-07-21 15:10:04 +0200

    Total refactoring (store types)

--- src/components/OperationList.vue
+++ src/components/OperationList.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   v-container(fluid).pl-4.pr-4
     v-layout(row wrap).stretch
-      v-flex(xs12 sm6 md4 lg3 v-ripple='' relative v-if="item._display" v-for="(item, key) in items" :key="item._id" @click="setOperation(item)").operation--block
+      v-flex(xs12 sm6 md4 lg3 v-ripple='' relative v-if="item._display" v-for="(item, key) in items" :key="item._id" @click="SET_OPERATION(item)").operation--block
         v-divider
         .pt-1.pb-1
           app-operation(:o="items[key]")
