40bda6b966c96a21c573bdcd4ea22e9b73e20a98
Jan Forst <ensonador@gmail.com>
2017-07-23 01:26:06 +0200

    Pending disabled

--- src/components/pages/ExplorerPage.vue
+++ src/components/pages/ExplorerPage.vue
@@ -9,7 +9,7 @@
     app-header-dialog
     app-proxy-dialog
     app-security-dialog
-    v-btn(v-if="FAB", @click.native.stop="FAB_METHOD && FAB_METHOD()" fab fixed bottom right secondary class="white--text fab--on-top")
+    v-btn(v-if="FAB", @click.native.stop="FAB_METHOD && FAB_METHOD()" fab fixed bottom right secondary :class="'white--text fab--on-top' + (FAB_PENDING ? ' fab--on-top--disabled' : '')")
       v-progress-circular(v-if="FAB_PENDING" indeterminate :width="3")
       v-icon(v-if="!FAB_PENDING" ) play_arrow
 </template>
@@ -48,4 +48,9 @@
   .fab--on-top
     z-index 4
     position fixed !important
+
+  .fab--on-top.fab--on-top--disabled
+    opacity 0.75
+    pointer-events none
+    box-shadow none
 </style>
