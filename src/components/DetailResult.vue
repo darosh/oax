5556b8b577be670c178830714b7aef50ae2e9885
Jan Forst <ensonador@gmail.com>
2017-07-22 21:21:30 +0200

    Result update

--- src/components/DetailResult.vue
+++ src/components/DetailResult.vue
@@ -1,13 +1,23 @@
 <template lang="pug">
-  div
-    | RESULT
-    | {{item._result}}
-    | {{item._error}}
+  div(v-if="item._result")
+    .subheader Request URL
+    a.pl-3.pl-3(:href="item._result.config.url" target="_blank") {{item._result.config.url}}
+    .subheader Response status
+    div.pl-3.pl-3
+      app-response(:code="item._result.status")
+      |  {{item._result.statusText}}
+    .subheader Response headers
+    div.pl-3.pl-3(v-for="(header, headerName) in item._result.headers", :key="headerName") {{headerName}} {{header}}
+    .subheader Response body
+    pre.pl-3.pl-3 {{item._result.data}}
 </template>
 
 <script>
   export default {
-    props: ['item']
+    props: ['item'],
+    components: {
+      appResponse: () => import('./Response')
+    }
   }
 </script>
 
