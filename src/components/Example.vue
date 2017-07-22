74de829501499adfabb64be08a997675139e3b21
Jan Forst <ensonador@gmail.com>
2017-07-22 12:06:05 +0200

    Implementing Example component

--- src/components/Example.vue
+++ src/components/Example.vue
@@ -1,9 +1,34 @@
 <template lang="pug">
-  div {{item}}
+  span
+    span(v-if="item.type === 'array'")
+      | [
+      app-example(:item="item.items", :level="level + 1")
+      | ]
+    span(v-else-if="item.type === 'object'")
+      | {
+      br
+      div(v-for="(prop, propName) in item.properties") {{space(level+1)}}
+        b "{{propName}}":&#32;
+        app-example(:item="prop", :level="level + 1")
+      | {{space(level)}}}
+    span(v-else) {{value}}
 </template>
 
 <script>
+  import { value } from '../models/oas/schema'
+
   export default {
-    props: ['item']
+    name: 'app-example',
+    props: {item: {}, level: {default: 0}},
+    computed: {
+      value () {
+        return JSON.stringify(value(this.item))
+      }
+    },
+    methods: {
+      space (level) {
+        return '  '.repeat(level >= 0 ? level : 0)
+      }
+    }
   }
 </script>
