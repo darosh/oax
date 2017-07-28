e951803755e117093885de9109866bf4ad071d28
Jan Forst <ensonador@gmail.com>
2017-07-28 15:01:50 +0200

    YQL https

--- src/services/yql-proxy.js
+++ src/services/yql-proxy.js
@@ -5,7 +5,7 @@ export default function yqlProxy (url) {
 }
 
 function yql (url) {
-  return 'http://query.yahooapis.com/v1/public/yql?q=' +
+  return 'https://query.yahooapis.com/v1/public/yql?q=' +
     encodeURIComponent('select * from json where url="' + url + '"') +
     '&format=json'
 }
