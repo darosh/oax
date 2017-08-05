59368400c23a5f8b71bed9708f531ec782acb796
Jan Forst <ensonador@gmail.com>
2017-08-05 07:12:23 +0200

    highlight param

--- src/components/elements/MethodBlock.vue
+++ src/components/elements/MethodBlock.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   div.operation(:class="{deprecated: item.deprecated}")
     app-method(:item="item._method")
-    span.operation--message.pl-1 {{item._pathName}}
+    .operation--message.pl-1(v-html="params(item._pathName)")
 </template>
 
 <script>
@@ -9,9 +9,19 @@
     components: {
       appMethod: () => import('./Method')
     },
-    props: ['item']
+    props: ['item'],
+    methods: {
+      params (value) {
+        return value.replace(/\//g, '/\u200b').replace(/{/g, '<span class="param text-success">').replace(/}/g, '</span>')
+      }
+    }
   }
 </script>
+
+<style lang="stylus">
+  .secondary .param
+    border-color rgba(255, 255, 255, 0.5) !important
+</style>
 
 <style scoped lang="stylus">
   .deprecated
@@ -26,5 +36,13 @@
 
   .operation--message
     padding-top 6px
-    word-break break-all
+    width calc(100% - 64px)
+
+  >>> .param
+    opacity 0.87
+    background-color rgba(128, 128, 128, 0.5)
+    border-radius 4px
+    padding 0 2px
+    margin 0 1px
+    border 1px solid rgba(160, 160, 160, 0.5)
 </style>
