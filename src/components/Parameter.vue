df710fb8c8e0282b2bd480e46aab4e7f8bd7b822
Jan Forst <ensonador@gmail.com>
2017-08-10 16:53:03 +0200

    fixes

--- src/components/Parameter.vue
+++ src/components/Parameter.vue
@@ -2,7 +2,7 @@
   div
     v-text-field(v-model="value" v-if="!item.enum && (type === 'string')", :label="item.name", :hint="item.description", :required="item.required" persistent-hint)
     v-text-field(v-model="value" v-else-if="!item.enum && (type === 'number')" type="number", :label="item.name", :hint="item.description", :required="item.required" persistent-hint)
-    v-text-field(v-model="value" v-else-if="!type && item.schema" multi-line :label="item.name", :hint="item.description", :required="item.required" persistent-hint)
+    v-text-field(v-model="value" v-else-if="!type && item.schema" multi-line :rows="3", :label="item.name", :hint="item.description", :required="item.required" persistent-hint)
     v-text-field(v-model="value" v-else-if="type === 'file'" type="file", :label="item.name", :hint="item.description", :required="item.required" persistent-hint)
     <!--TODO: Switch to primary checkbox colors in multiple select-->
     v-select(v-model="value" v-else-if="(type === 'array')", :items="item.items.enum" multiple :label="item.name", :hint="item.description", :required="item.required" persistent-hint)
