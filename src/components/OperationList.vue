c9223060ede4dd2647e81b549924a960843b0aba
Jan Forst <ensonador@gmail.com>
2017-08-10 23:05:24 +0200

    fixes

--- src/components/OperationList.vue
+++ src/components/OperationList.vue
@@ -2,11 +2,12 @@
   v-container(fluid).pl-4.pr-4.pa-3-sm
     v-layout(row wrap).stretch
       v-flex(xs12 sm6 md4 lg3 v-ripple="" relative v-if="item._display" v-for="(item, key) in OPERATIONS", :key="item._id", @click.stop="SET_OPERATION(item)", :class="{'secondary white--text': OPERATION === OPERATIONS[key]}").hover--block
+        v-divider.hidden-xs-only
         .pt-1.pb-1.plr-3-sm
           app-operation(:item="OPERATIONS[key]")
           div(v-if="IS_DESCRIPTION && item.summary")
             .pt-1.pr-1.body-1.md(style="padding-left: 68px" v-html="md(item, 'summary')")
-        v-divider
+        v-divider.hidden-sm-and-up
 </template>
 
 <script>
