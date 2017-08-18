cbad760c9ed1db11e0355cfa3ff45abb337a5ab3
Jan Forst <ensonador@gmail.com>
2017-08-18 22:15:45 +0200

    fab

--- src/components/FAB.vue
+++ src/components/FAB.vue
@@ -1,8 +1,11 @@
 <template lang="pug">
-  v-btn(v-if="FAB", @click.native.stop="FAB_METHOD && FAB_METHOD()" fab fixed bottom right secondary :class="'white--text fab--on-top' + ((FAB_PENDING || !ONLINE) ? ' fab--on-top--disabled' : '')")
-    v-progress-circular(v-if="FAB_PENDING" indeterminate :width="3")
-    v-icon(v-if="!FAB_PENDING && ONLINE" ) play_arrow
-    v-icon(v-if="!ONLINE && !FAB_PENDING" ) do_not_disturb_alt
+  div
+    v-btn(v-if="FAB", @click.native.stop="FAB_METHOD && FAB_METHOD()" fab fixed bottom right accent :class="'fab--second fab--on-top' + ((FAB_PENDING || !ONLINE) ? ' fab--on-top--disabled' : '')")
+      v-icon lock
+    v-btn(v-if="FAB", @click.native.stop="FAB_METHOD && FAB_METHOD()" fab fixed bottom right secondary :class="'white--text fab--on-top' + ((FAB_PENDING || !ONLINE) ? ' fab--on-top--disabled' : '')")
+      v-progress-circular(v-if="FAB_PENDING" indeterminate :width="3")
+      v-icon(v-if="!FAB_PENDING && ONLINE" ) play_arrow
+      v-icon(v-if="!ONLINE && !FAB_PENDING" ) do_not_disturb_alt
 </template>
 
 <script>
@@ -30,4 +33,7 @@
     opacity 0.75
     pointer-events none
     box-shadow none
+
+  .fab--second
+    bottom 88px
 </style>
