0c83e4af10f9720f54f9714587af9e9cbf74b311
Jan Forst <ensonador@gmail.com>
2017-08-27 01:24:21 +0200

    markdown... switching to directive ...fix

--- src/directives/markdown.js
+++ src/directives/markdown.js
@@ -1,23 +1,37 @@
 import { markdown } from '../workers/main'
 
+function update (el, binding) {
+  const name = binding.modifiers.summary ? 'summary' : 'description'
+
+  if (binding.value[name]) {
+    const mname = '_md_' + name
+
+    if (binding.value[mname]) {
+      el.innerHTML = binding.value[mname]
+      el.className += ' markdown-body'
+    } else {
+      el.className += ' markdown-body'
+      markdown(binding.value[name]).then(md => {
+        el.innerHTML = binding.value[mname] = md
+      })
+    }
+  }
+}
+
 export default {
-  inserted: function (el, binding, vnode) {
+  update (el, binding) {
+    if (binding.value !== binding.oldValue) {
+      update(el, binding)
+    }
+  },
+  // componentUpdated (el, binding) {
+  //   if (binding.value !== binding.oldValue) {
+  //     update(el, binding)
+  //   }
+  // },
+  inserted: function (el, binding) {
     if (binding.value) {
-      const name = binding.modifiers.summary ? 'summary' : 'description'
-
-      if (binding.value[name]) {
-        const mname = '_md_' + name
-
-        if (binding.value[mname]) {
-          el.innerHTML = binding.value[mname]
-          el.className += ' markdown-body'
-        } else {
-          el.className += ' markdown-body'
-          markdown(binding.value[name]).then(md => {
-            el.innerHTML = binding.value[mname] = md
-          })
-        }
-      }
+      update(el, binding)
     }
   }
 }
