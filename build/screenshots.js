1588a17cf5763c10a8a0930c5bd0006291a21d5a
Jan Forst <ensonador@gmail.com>
2017-08-29 18:25:05 +0200

    screenshots

--- build/screenshots.js
+++ build/screenshots.js
@@ -4,71 +4,85 @@ const screenshots = 'screenshots/'
 const W = 888
 const W2 = W * 2
 const M = W / 8
-const M2 = M * 2;
-const M4 = M * 4;
-const T = 320;
+const M2 = M * 2
+const M3 = M * 3
+const M4 = M * 4
+const M6 = M * 6
+const T = 320
+
+let counter = 0
+
+function path (name) {
+  return screenshots + '/' + ++counter + '_' + name + '.png'
+}
 
 (async () => {
   const browser = await puppeteer.launch()
 
-  const page = await browser.newPage()
-  await page.setViewport({width: W2, height: W})
+  let page = await browser.newPage()
 
-  // Loading
-  await page.goto('http://localhost:8080/', {waitUntil: 'networkidle'})
-  await page.screenshot({path: screenshots + 'loading.png'})
-  await page.setViewport({width: W2, height: M4})
+  async function test (dark, url) {
+    await page.setViewport({width: M4, height: M6})
 
-  // Grouped
-  await page.waitFor('.expansion-panel')
-  page.evaluate(_ => {
-    window.document.getElementsByTagName('main')[0].scrollBy(0, 400)
-  })
-  await page.screenshot({path: screenshots + 'grouped.png'})
+    // Loading
+    await page.goto(url, {waitUntil: 'networkidle'})
+    if (dark) {
+      page.evaluate(_ => {
+        window.OAX.$store.commit('TOGGLE_DARK')
+      })
+    }
+    await page.screenshot({path: path('loading')})
 
-  // Panel
-  let el = await page.$('.operation')
-  await el.click()
-  await page.waitFor(T)
-  await page.screenshot({path: screenshots + 'panel.png'})
+    // Desktop
+    await page.waitFor(T)
+    await page.setViewport({width: W2, height: M6})
+    await page.screenshot({path: path('desktop')})
 
-  // Dark
-  el = await page.$('.toolbar .menu:last-of-type button')
-  await el.click()
-  page.evaluate(_ => {
-    window.OAX.$store.commit('TOGGLE_DARK')
-  })
-  await page.screenshot({path: screenshots + 'dark.png'})
-  page.evaluate(_ => {
-    window.OAX.$store.commit('TOGGLE_DARK')
-  })
+    // Grouped
+    await page.setViewport({width: W2, height: M6})
+    await page.waitFor('.expansion-panel')
+    page.evaluate(_ => {
+      window.document.getElementsByTagName('main')[0].scrollBy(0, 400)
+    })
+    await page.screenshot({path: path('grouped')})
 
-  // Dialog
-  el = await page.$('.toolbar--scroll .operation')
-  await el.click()
-  await page.waitFor(T)
-  await page.screenshot({
-    path: screenshots + 'method-dialog.png',
-    clip: {x: 400, y: 150, width: 400, height: 300}
-  })
+    // Panel
+    let el = await page.$('.operation')
+    await el.click()
+    await page.waitFor(T)
+    await page.screenshot({path: path('panel')})
 
-  el = await page.$('button[data-v-5c96e174]:last-child')
-  await el.click()
+    // Dialog
+    await page.setViewport({width: W, height: M4})
+    el = await page.$('.toolbar--scroll .operation')
+    await el.click()
+    await page.waitFor(T)
+    await page.screenshot({
+      path: path('method-dialog')
+    })
 
-  // Markdown
-  await page.setViewport({width: M4, height: 1900})
-  await page.goto(
-    'http://localhost:8080/#/?url=.%2Fstatic%2Ftest%2Fmarkdown%2Fswagger-gfm.json',
-    {waitUntil: 'networkidle'})
-  await page.waitFor('.expansion-panel')
-  await page.screenshot({
-    path: screenshots + 'markdown.png',
-    clip: {x: 0, y: 0, width: M4, height: 800}
-  })
-  await page.screenshot({
-    path: screenshots + 'blocks.png',
-    clip: {x: 0, y: 1030, width: M4, height: 800}
-  })
+    el = await page.$('button[data-v-5c96e174]:last-child')
+    await el.click()
+
+    // Markdown
+    page = await browser.newPage()
+    await page.setViewport({width: M4, height: 1900})
+    await page.goto(
+      'http://localhost:8080/#/?url=.%2Fstatic%2Ftest%2Fmarkdown%2Fswagger-gfm.json',
+      {waitUntil: 'networkidle'})
+    await page.waitFor('.expansion-panel')
+    await page.screenshot({
+      path: path('markdown'),
+      clip: {x: 0, y: 0, width: M4, height: 800}
+    })
+    await page.screenshot({
+      path: path('blocks'),
+      clip: {x: 0, y: 1030, width: M4, height: 800}
+    })
+  }
+
+  await test(true, 'http://localhost:8080/#/?url=https%3A%2F%2Fdarosh.github.io%2Fangular-swagger-ui-material%2Fswagger-drupal.json')
+  await test(false, 'http://localhost:8080/')
 
   browser.close()
 })()
