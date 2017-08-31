db1cebf7330ed8063123e09d5fb1e6f6ace53ff5
Jan Forst <ensonador@gmail.com>
2017-08-31 18:34:58 +0200

    log update

--- src/components/Log.vue
+++ src/components/Log.vue
@@ -2,7 +2,7 @@
   div
     div(v-for="(i, k) in items", :style="{opacity: (k && !log) ? Math.min(0.8, Math.max(0.33, 1 / (k / 2))) : 1, 'margin-bottom': !k && !log ? '16px' : 0}" style="min-width: 260px; position: relative")
       span(v-if="i.elapsed || k" style="inline-block; position: absolute; right: 0px; font-family: 'Roboto Mono', monospaced") +{{i.elapsed}} ms
-      span(v-if="i.elapsed > 200" style="inline-block; position: absolute; right: -64px; font-family: 'Roboto Mono', monospaced;") [slow]
+      b.error--text(v-if="i.elapsed > 200" style="inline-block; position: absolute; right: -16px; font-family: 'Roboto Mono', monospaced;") !
       <!--span(v-if="log && (i.elapsed || k)" style="inline-block; position: absolute; right: -64px; font-family: 'Roboto Mono', monospaced") {{Math.round(i.done * 100)}}%-->
       span(style="inline-block; font-size: 16px") {{i.text}}
         span(v-if="!k && !i.elapsed") &hellip;
