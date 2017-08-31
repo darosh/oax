38096d353f5d9a7edd7dd1371269a1f0c11575bd
Jan Forst <ensonador@gmail.com>
2017-08-31 07:07:22 +0200

    markdown

--- src/directives/markdown.js
+++ src/directives/markdown.js
@@ -1,21 +1,5 @@
 import { markdown, summary } from '../workers/main'
 
-function updated (el) {
-  const blocks = el.querySelectorAll('pre > code[class*="language-"]')
-
-  if (blocks.length) {
-    for (const block of blocks) {
-      const lc = block.className.split(' ')
-        .filter(c => c.indexOf('language-') > -1)
-
-      if (lc.length) {
-        // const lang = lc[0].split('-')[1]
-        // block.textContent = lang
-      }
-    }
-  }
-}
-
 function update (el, binding) {
   const name = binding.modifiers.summary ? 'summary' : 'description'
   const v = binding.value
@@ -27,13 +11,11 @@ function update (el, binding) {
     if (v[mname]) {
       el.innerHTML = v[mname]
       el.className += ' markdown'
-      updated(el)
     } else {
       el.className += ' markdown';
       (v[jname] || (v[jname] = markdown(v[name]))).then(md => {
         delete v[jname]
         el.innerHTML = v[mname] = md
-        updated(el)
       })
     }
   } else if (binding.modifiers.summary && v.description) {
