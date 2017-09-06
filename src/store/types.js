6da7b461835748777b21fb7025d8fcecf43614e1
Jan Forst <ensonador@gmail.com>
2017-09-06 09:31:41 +0200

    refactoring types

--- src/store/types.js
+++ src/store/types.js
@@ -1,13 +1,29 @@
-// recent
-export const RECENT = 'RECENT'
-export const RECENT_SET_UNSHIFT = 'RECENT_SET_UNSHIFT'
-export const RECENT_SET_REMOVE = 'RECENT_SET_REMOVE'
-
 // app
 export const APP_VERSION = 'APP_VERSION'
 export const APP_API_PAGE = 'APP_API_PAGE'
 export const APP_PATH = 'APP_PATH'
 export const APP_PAGE_NAME = 'APP_PAGE_NAME'
+
+// spec
+export const SPEC = 'SPEC'
+export const SPEC_JSON = 'SPEC_JSON'
+export const SPEC_METAS = 'SPEC_METAS'
+export const SPEC_OPERATION = 'SPEC_OPERATION'
+export const SPEC_OPERATIONS = 'SPEC_OPERATIONS'
+export const SPEC_RESOURCES = 'SPEC_RESOURCES'
+export const SPEC_URL = 'SPEC_URL'
+// spec set
+export const SPEC_SET = 'SPEC_SET'
+export const SPEC_SET_EDIT_JSON = 'SPEC_SET_EDIT_JSON'
+export const SPEC_SET_FILTER_RESOURCES = 'SPEC_SET_FILTER_RESOURCES'
+export const SPEC_SET_LOAD_URL = 'SPEC_SET_LOAD_URL'
+export const SPEC_SET_NEXT_OPERATION = 'SPEC_SET_NEXT_OPERATION'
+export const SPEC_SET_OPERATION = 'SPEC_SET_OPERATION'
+export const SPEC_SET_PREV_OPERATION = 'SPEC_SET_PREV_OPERATION'
+export const SPEC_SET_RESOURCE = 'SPEC_SET_RESOURCE'
+export const SPEC_SET_RESOURCES = 'SPEC_SET_RESOURCES'
+export const SPEC_SET_RESULT = 'SPEC_SET_RESULT'
+export const SPEC_SET_VALUE = 'SPEC_SET_VALUE'
 
 // ui
 export const UI_DIALOG = 'UI_DIALOG'
@@ -23,7 +39,7 @@ export const UI_ONLINE = 'UI_ONLINE'
 export const UI_RIGHT_DRAWER = 'UI_RIGHT_DRAWER'
 export const UI_TAB = 'UI_TAB'
 export const UI_WIDTH = 'UI_WIDTH'
-
+// ui set
 export const UI_SET_DIALOG = 'UI_SET_DIALOG'
 export const UI_SET_DRAWER = 'UI_SET_DRAWER'
 export const UI_SET_ERROR = 'UI_SET_ERROR'
@@ -42,7 +58,7 @@ export const VIEW_PATH = 'VIEW_PATH'
 export const VIEW_SUMMARY = 'VIEW_SUMMARY'
 export const VIEW_VIEW = 'VIEW_VIEW'
 export const VIEW_WIDE = 'VIEW_WIDE'
-
+// view set
 export const VIEW_SET_DARK = 'VIEW_SET_DARK'
 export const VIEW_SET_PATH = 'VIEW_SET_PATH'
 export const VIEW_SET_SUMMARY = 'VIEW_SET_SUMMARY'
@@ -51,32 +67,18 @@ export const VIEW_SET_WIDE = 'VIEW_SET_WIDE'
 
 // settings
 export const SETTINGS_SEARCH = 'SETTINGS_SEARCH'
-export const SETTINGS_SET_SEARCH = 'SETTINGS_SET_SEARCH'
 export const SETTINGS_URL = 'SETTINGS_URL'
+// settings set
 export const SETTINGS_SET_URL = 'SETTINGS_SET_URL'
+export const SETTINGS_SET_SEARCH = 'SETTINGS_SET_SEARCH'
 
 // apis
 export const APIS = 'APIS'
 export const APIS_SET = 'APIS_SET'
 export const APIS_RUN_LOAD = 'APIS_RUN_LOAD'
 
-// spec
-export const SPEC = 'SPEC'
-export const SPEC_JSON = 'SPEC_JSON'
-export const SPEC_METAS = 'SPEC_METAS'
-export const SPEC_OPERATION = 'SPEC_OPERATION'
-export const SPEC_OPERATIONS = 'SPEC_OPERATIONS'
-export const SPEC_RESOURCES = 'SPEC_RESOURCES'
-export const SPEC_URL = 'SPEC_URL'
-
-export const SPEC_SET = 'SPEC_SET'
-export const SPEC_SET_EDIT_JSON = 'SPEC_SET_EDIT_JSON'
-export const SPEC_SET_FILTER_RESOURCES = 'SPEC_SET_FILTER_RESOURCES'
-export const SPEC_SET_LOAD_URL = 'SPEC_SET_LOAD_URL'
-export const SPEC_SET_NEXT_OPERATION = 'SPEC_SET_NEXT_OPERATION'
-export const SPEC_SET_OPERATION = 'SPEC_SET_OPERATION'
-export const SPEC_SET_PREV_OPERATION = 'SPEC_SET_PREV_OPERATION'
-export const SPEC_SET_RESOURCE = 'SPEC_SET_RESOURCE'
-export const SPEC_SET_RESOURCES = 'SPEC_SET_RESOURCES'
-export const SPEC_SET_RESULT = 'SPEC_SET_RESULT'
-export const SPEC_SET_VALUE = 'SPEC_SET_VALUE'
+// recent
+export const RECENT = 'RECENT'
+// recent set
+export const RECENT_SET_UNSHIFT = 'RECENT_SET_UNSHIFT'
+export const RECENT_SET_REMOVE = 'RECENT_SET_REMOVE'
