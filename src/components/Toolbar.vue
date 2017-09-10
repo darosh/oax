782171629fbe3104536e15a3c455c149f715c3f0
Jan Forst <ensonador@gmail.com>
2017-09-10 14:57:10 +0200

    gh link

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -37,6 +37,7 @@
         v-btn.hidden-xs-only(flat href="#/statuses" tag="a") Statuses
         v-btn.hidden-xs-only(flat href="#/headers" tag="a") Headers
         v-btn.hidden-xs-only(flat href="#/about" tag="a") About
+        v-btn.hidden-xs-only(flat href="https://github.com/darosh/oax" tag="a" target="_blank") GitHub
 
       v-menu.hidden-sm-and-up(bottom left)
         v-btn(icon slot="activator")
