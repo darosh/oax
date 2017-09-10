e22efbd4be61ed3d28b3ede4cd76a85bf3e73dba
Jan Forst <ensonador@gmail.com>
2017-09-10 12:18:50 +0200

    project reorg

--- build/doc/test-links.js
+++ build/doc/test-links.js
@@ -3,10 +3,10 @@ const tools = require('../../config/doc/tools.json')
 const tests = require('openapi-samples')
 
 const template = require('lodash.template')
-const tmpl = fs.readFileSync('config/test.md', 'utf8')
-const targetPath = 'RESEARCH.md'
-const target = fs.readFileSync(targetPath, 'utf8')
+const tmpl = fs.readFileSync('config/doc/test-links.md', 'utf8')
+const targetPath = 'doc/RESEARCH.md'
+// const target = fs.readFileSync(targetPath, 'utf8')
 
 const compiled = template(tmpl)
 
-fs.writeFileSync('___' + targetPath, compiled({tools, tests}))
+fs.writeFileSync(targetPath + '.temp', compiled({tools, tests}))
