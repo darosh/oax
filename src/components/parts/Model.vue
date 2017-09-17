b89d13f1e9116658761be479c24421a5794810a0
Jan Forst <ensonador@gmail.com>
2017-09-17 09:52:40 +0200

    better schema

--- src/components/parts/Model.vue
+++ src/components/parts/Model.vue
@@ -16,7 +16,7 @@
         app-model(v-if="value || expanded[propName] || (expanded[propName] === null)", :item="prop", :level="level + 1", v-model="value")
         span(v-else class="click", @click.stop="expanded[propName] = !expanded[propName]") &hellip;
     | }
-  span(v-else :class="{'cm-string': type === 'string', 'cm-atom': type === 'boolean', 'cm-number': type === 'number'}") {{t}}
+  span(v-else :class="{'cm-string': type === 'string', 'cm-atom': type === 'boolean', 'cm-number': type === 'number'}") {{item.format || item.type || t}}
 </template>
 
 <script>
