572c7c2cc37d3cb3ce1893e400049a608ade8313
Jan Forst <ensonador@gmail.com>
2017-09-18 07:36:42 +0200

    fix route to

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
+  mode: window.location.hostname === 'localhost' ? 'history' : 'hash',
   routes: [
     {
       path: '/',
