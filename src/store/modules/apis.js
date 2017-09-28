2fbd5722161e1d00b2f24a2777d43f671c337ba4
Jan Forst <ensonador@gmail.com>
2017-09-28 23:47:22 +0200

    apis.js update

--- src/store/modules/apis.js
+++ src/store/modules/apis.js
@@ -19,9 +19,11 @@ export const mutations = {
   }
 }
 
+let lastObj = null
+
 export const actions = {
   [types.APIS_RUN_LOAD] ({commit, getters}, {next, search} = {}) {
-    const obj = getters[types.APIS_COLLECTION_OBJECT]
+    const obj = lastObj = getters[types.APIS_COLLECTION_OBJECT]
 
     if (obj.loadingApis) {
       return
@@ -32,38 +34,36 @@ export const actions = {
 
     obj.loadingApis = true
 
-    if (!next) {
-      commit(types.APIS_SET, {apis: null, categories: null})
-    }
-
     let url
 
-    if (obj.search && search) {
-      if (obj.lastSearch !== search) {
-        commit(types.APIS_SET, {apis: null, categories: null})
-        obj.loaded = null
-      }
-
-      url = obj.search(search)
+    if (obj.search && (obj.lastSearch !== search)) {
+      commit(types.APIS_SET, {apis: null, categories: null})
+      obj.loaded = null
+      obj.lastSearch = null
+      obj.next = null
+      url = search ? obj.search(search) : obj.base
     } else {
-      if (obj.lastSearch !== search) {
+      if (!next) {
         commit(types.APIS_SET, {apis: null, categories: null})
-        obj.lastSearch = null
-        obj.next = null
-        obj.loaded = null
       }
 
       url = obj.next || obj.base
     }
 
     axios.get(url).then(res => {
-      if ((!search && !obj.lastSearch) || (search === obj.lastSearch)) {
-        const data = res.data
-        const {apis, categories} = obj.transform(data, (obj.loaded && obj.loaded.apis) || [])
-        obj.loadingApis = false
-        obj.loaded = {apis, categories}
-        commit(types.APIS_SET, {apis, categories})
+      if (obj !== lastObj) {
+        return
       }
+
+      if (obj.search && (search || obj.lastSearch) && (search !== obj.lastSearch)) {
+        return
+      }
+
+      const data = res.data
+      const {apis, categories} = obj.transform(data, (obj.loaded && obj.loaded.apis) || [])
+      obj.loadingApis = false
+      obj.loaded = {apis, categories}
+      commit(types.APIS_SET, {apis, categories})
     })
   }
 }
