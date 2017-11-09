3b3461f04e28a829f3b0821887ed85fa546a2063
Jan Forst <ensonador@gmail.com>
2017-11-09 12:24:55 +0100

    something wrong with custom d3 rollup :(

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,12 @@
+import ascii from 'rollup-plugin-ascii'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 
 export default {
+  extend: true,
   plugins: [
     resolve(),
+    ascii(),
     commonjs()
   ]
 }
