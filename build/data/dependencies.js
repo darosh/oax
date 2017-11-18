74990088cba7fe50f0af3a694e489ef093c25a3a
Jan Forst <ensonador@gmail.com>
2017-11-18 08:55:56 +0100

    acknowledgement generator fix

--- build/data/dependencies.js
+++ build/data/dependencies.js
@@ -5,7 +5,7 @@ const pkg = require('../../package.json')
 const deps = pkg.dependencies
 
 Object.keys(deps).forEach(d => {
-  if (d[0] === '@' || (deps[d].indexOf('github:') === 0)) {
+  if (d[0] === '@' || (deps[d].startsWith('github:') === 0)) {
     delete deps[d]
   } else {
     deps[d] = deps[d].replace('^', '').replace(/(.*)\.[^.]+/, '$1')
