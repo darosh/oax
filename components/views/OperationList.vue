87a5813b0ef0a2bea7dd30d493e97a620adac940
Jan Forst <ensonador@gmail.com>
2017-11-18 22:45:43 +0100

    fixing hover transition delay

--- components/views/OperationList.vue
+++ components/views/OperationList.vue
@@ -3,13 +3,14 @@
     .hidden-sm-and-up.mt-2
     //v-divider.hidden-sm-and-up
     v-layout(row wrap :class="{'dividers-bottom': VIEW_SUMMARY && VIEW_PATH}").stretch
-      .flex(:style="{'flex-basis': $innerMin(SPEC_OPERATIONS.length), 'max-width': $innerMin(SPEC_OPERATIONS.length)}",  v-ripple="true" relative v-if="item._._display" v-for="(item, key) in SPEC_OPERATIONS", :key="item._id", @click.stop="SPEC_SET_OPERATION(item)", :class="{'secondary white--text': SPEC_OPERATION === SPEC_OPERATIONS[key]}").hover--block
-        //v-divider.hidden-xs-only
-        .pt-1.pb-1.plr-3-sm
-          app-operation(:sum="!VIEW_PATH", :item="SPEC_OPERATIONS[key]")
-          div(v-if="VIEW_SUMMARY && VIEW_PATH && (item._.summary || item._.description)")
-            .pt-1.pr-1(style="padding-left: 68px" v-markdown.summary="item._")
-        //v-divider.hidden-sm-and-up
+      .flex(:style="{'flex-basis': $innerMin(SPEC_OPERATIONS.length), 'max-width': $innerMin(SPEC_OPERATIONS.length)}" v-if="item._._display" v-for="(item, key) in SPEC_OPERATIONS", :key="item._id")
+        .hover--block(v-ripple="true" relative, :class="{'secondary white--text': SPEC_OPERATION === SPEC_OPERATIONS[key]}", @click.stop="SPEC_SET_OPERATION(item)")
+          //v-divider.hidden-xs-only
+          .pt-1.pb-1.plr-3-sm
+            app-operation(:sum="!VIEW_PATH", :item="SPEC_OPERATIONS[key]")
+            div(v-if="VIEW_SUMMARY && VIEW_PATH && (item._.summary || item._.description)")
+              .pt-1.pr-1(style="padding-left: 68px" v-markdown.summary="item._")
+          //v-divider.hidden-sm-and-up
     .hidden-sm-and-up.mt-2
 </template>
 
