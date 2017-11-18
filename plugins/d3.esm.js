ea3ada5976b63beb5a37971f060b47d41e952882
Jan Forst <ensonador@gmail.com>
2017-11-18 16:05:14 +0100

    removing d3.select

--- plugins/d3.esm.js
+++ plugins/d3.esm.js
@@ -1,4 +1,3 @@
-import { default as select } from 'd3-selection/src/select'
 import { default as line } from 'd3-shape/src/line'
 
 export { format } from 'd3-format/src/defaultLocale'
@@ -7,14 +6,10 @@ export { default as pie } from 'd3-shape/src/pie'
 export { default as extent } from 'd3-array/src/extent'
 export { default as histogram } from 'd3-array/src/histogram'
 export { default as scaleLinear } from 'd3-scale/src/linear'
-// export { scaleLinear } from 'd3-scale'
-export { default as select } from 'd3-selection/src/select'
-// export { default as curveBasis } from 'd3-shape/src/curve/basis'
 export { monotoneX } from 'd3-shape/src/curve/monotone'
 export { default as line } from 'd3-shape/src/line'
 
 const d3 = {
-  select,
   line
 }
 
