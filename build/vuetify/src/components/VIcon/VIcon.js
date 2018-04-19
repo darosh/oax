c2059bcbecc5c10688cee4db6c64cce2438c8ac2
darosh <ensonador@gmail.com>
2018-04-19 15:01:30 +0200

    updating vuetify

--- build/vuetify/src/components/VIcon/VIcon.js
+++ build/vuetify/src/components/VIcon/VIcon.js
@@ -1,65 +1,104 @@
-require('../../stylus/components/_icons.styl')
+import '../../stylus/components/_icons.styl'
 
 import Themeable from '../../mixins/themeable'
-import Contextualable from '../../mixins/contextualable'
-import Vue from 'vue'
+import Colorable from '../../mixins/colorable'
+
+const SIZE_MAP = {
+  small: '16px',
+  default: '24px',
+  medium: '28px',
+  large: '36px',
+  xLarge: '40px'
+}
+
+function isFontAwesome5 (iconType) {
+  return ['fas', 'far', 'fal', 'fab'].some(val => iconType.includes(val))
+}
 
 export default {
   name: 'v-icon',
 
   functional: true,
 
-  mixins: [Themeable, Contextualable],
+  mixins: [Colorable, Themeable],
 
   props: {
     disabled: Boolean,
-    fa: Boolean,
-    mdi: Boolean,
     large: Boolean,
     left: Boolean,
     medium: Boolean,
     right: Boolean,
+    size: {
+      type: [Number, String]
+    },
+    small: Boolean,
     xLarge: Boolean
   },
 
-  render (h, {props, data, children = []}) {
-    let iconName = ''
+  // eslint-disable-next-line max-statements
+  render (h, { props, data, children = [] }) {
+    const { small, medium, large, xLarge } = props
+    const sizes = { small, medium, large, xLarge }
+    const explicitSize = Object.keys(sizes).find(key => sizes[key] && key)
+    const fontSize = (explicitSize && SIZE_MAP[explicitSize]) || props.size
 
-    if (children.length) {
-      iconName = children.pop().text
-    } else if (data.domProps && data.domProps.textContent) {
-      iconName = data.domProps.textContent
+    if (fontSize) data.style = { fontSize, ...data.style }
+
+    let iconName = ''
+    if (children.length) iconName = children.pop().text
+    // Support usage of v-text and v-html
+    else if (data.domProps) {
+      iconName = data.domProps.textContent ||
+        data.domProps.innerHTML ||
+        iconName
+
+      // Remove nodes so it doesn't
+      // overwrite our changes
       delete data.domProps.textContent
-    } else if (data.domProps && data.domProps.innerHTML) {
-      iconName = data.domProps.innerHTML
       delete data.domProps.innerHTML
     }
 
-    data.staticClass = ('icon ' + (data.staticClass || '')).trim()
+    let iconType = 'material-icons'
+    // Material Icon delimiter is _
+    // https://material.io/icons/
+    const delimiterIndex = iconName.indexOf('-')
+    const isCustomIcon = delimiterIndex > -1
 
-    const classes = {
-      'icon--large': props.large,
+    if (isCustomIcon) {
+      iconType = iconName.slice(0, delimiterIndex)
+
+      if (isFontAwesome5(iconType)) iconType = ''
+    // Assume if not a custom icon
+    // is Material Icon font
+    } else children.push(iconName)
+
+    data.attrs = data.attrs || {}
+    if (!('aria-hidden' in data.attrs)) {
+      data.attrs['aria-hidden'] = true
+    }
+
+    const classes = Object.assign({
+      'icon--disabled': props.disabled,
       'icon--left': props.left,
-      'icon--medium': props.medium,
       'icon--right': props.right,
-      'icon--x-large': props.xLarge,
-      'primary--text': props.primary,
-      'secondary--text': props.secondary,
-      'success--text': props.success,
-      'info--text': props.info,
-      'warning--text': props.warning,
-      'error--text': props.error,
       'theme--dark': props.dark,
       'theme--light': props.light
-    }
+    }, props.color ? Colorable.methods.addTextColorClassChecks.call(props, {}, props.color) : {})
 
-    const iconClasses = Object.keys(classes).filter(k => classes[k]).join(' ')
-    iconClasses && (data.staticClass += ` ${iconClasses}`)
+    // Order classes
+    // * Component class
+    // * Vuetify classes
+    // * Icon Classes
+    data.staticClass = [
+      'icon',
+      data.staticClass,
+      Object.keys(classes).filter(k => classes[k]).join(' '),
+      iconType,
+      isCustomIcon ? iconName : null
+    ].reduce((prev, curr) => curr ? `${prev} ${curr}` : prev)
+      .trim()
 
-    if (props.disabled) {
-      data.attrs = data.attrs || {}
-      data.attrs.disabled = props.disabled
-    }
+    // return h('i', data, children)
 
     const svgData = {attrs: {}}
     svgData.attrs.width = 24
@@ -71,7 +110,19 @@ export default {
     const $icons = typeof window !== 'undefined' ? window.$icons : global.$icons
 
     if (!$icons || !$icons[iconName]) {
-      console.warn(`Missing icon ${iconName}.`)
+      let $missingIcons = typeof window !== 'undefined' ? window.$missingIcons : global.$missingIcons
+
+      if (!$missingIcons || !$missingIcons[iconName]) {
+        console.warn(`Missing icon ${iconName}.`)
+
+        if (typeof window !== 'undefined') {
+          $missingIcons = window.$missingIcons = window.$missingIcons || {}
+        } else {
+          $missingIcons = global.$missingIcons = global.$missingIcons || {}
+        }
+
+        $missingIcons[iconName] = true
+      }
     }
 
     const pathData = ($icons && $icons[iconName]) || 'M4 4 H 20 V 20 H 4 L 4 4'
